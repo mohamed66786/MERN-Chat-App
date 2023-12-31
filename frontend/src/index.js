63c62808943be1397aac7b8bf1c7a09c6c1d86e0
mohamed66786 <mohamedtarek7080@gmail.com>
2023-12-31 07:34:34 +0200

    add new edit

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { disapleReactDevTools } from "@fvilers/disable-react-devtools";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+disapleReactDevTools();
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
