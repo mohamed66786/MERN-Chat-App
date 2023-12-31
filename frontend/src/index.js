cc921367a85b8d91b986cf4689cca20474a0a9a0
mohamed66786 <mohamedtarek7080@gmail.com>
2024-01-01 00:54:34 +0200

    edit it to deploy

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App";
 import { disapleReactDevTools } from "@fvilers/disable-react-devtools";
 
-disapleReactDevTools();
+if (process.env.NODE_ENV === "production") disapleReactDevTools();
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
